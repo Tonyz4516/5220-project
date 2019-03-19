807538528d6a3efdd88bed889b9ae69887fd299e
Tony Z 4516 <44876222+Tonyz4516@users.noreply.github.com>
2019-03-19 15:42:42 -0400

    Update convert_agree_disagree.R

--- data_clean/convert_agree_disagree.R
+++ data_clean/convert_agree_disagree.R
@@ -1,1 +1,17 @@
-# convert agree / disagree cols
+# convert agree / disagree cols to numeric
+# source("data_clean.R")
+voter2
+
+# find keywords likely involving ordinal data
+keywords <- c("Agree", "Strongly", "Very", "Somewhat",
+              "Much", "more", "less", "same", "better", "worse") 
+
+find_them <- function(vec) {
+    l <- grep(paste(keywords, collapse=" | "), 
+                 vec, ignore.case = T)
+    result <- levels(factor(vec[l]))
+    if(length(result) != 0) return(result)
+}
+re <- unique(unlist(apply(voter2, 2, find_them)))
+
+# manual check them and select
