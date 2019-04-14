630c15ae64f58628764b748d99fb39c05ab2541e
TonyZ4516 <44876222+Tonyz4516@users.noreply.github.com>
2019-04-14 15:20:16 -0400

    4/14 update

--- spread_and_impute/impute.R
+++ spread_and_impute/impute.R
@@ -15,3 +15,9 @@ library(Amelia)
 voter_train_imp <- 
     amelia(voter_train, m=1, 
            parallel = "multicore", noms = "accurately_counted_2016")
+
+
+
+cl<-makeCluster(10,type="SOCK")
+a = clusterApply(cl, voter_train, function(x) )
+stopCluster(cl)
