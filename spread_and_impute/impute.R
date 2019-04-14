694c99b4e95ed8f481807755d5835c6ebdc87896
TonyZ4516 <44876222+Tonyz4516@users.noreply.github.com>
2019-04-13 20:55:19 -0400

    Update impute.R

--- spread_and_impute/impute.R
+++ spread_and_impute/impute.R
@@ -1,4 +1,17 @@
 # impute train
-library(mice)
-voter_train_imp <- mice(voter_train, m=5, maxit = 50,
-                        method = 'pmm', seed = 500)
+rm(list = ls())
+load("spread_and_impute/train_test.rda")
+
+loc = rep(0, 1292)
+for (i in 1:1292) {
+    if (class(voter_train[,i]) == "numeric") {
+        loc[i] = 1
+    }
+}
+voter_train = voter_train[,which(loc == 1)]
+voter_test = voter_test[,which(loc == 1)]
+
+library(Amelia)
+voter_train_imp <- 
+    amelia(voter_train, m=1, 
+           parallel = "multicore", noms = "accurately_counted_2016")
