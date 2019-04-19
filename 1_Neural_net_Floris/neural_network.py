ae6f9d7d3ed210ff23244b04fbdadcf146085017
FlorisWu <45719504+FlorisWu@users.noreply.github.com>
2019-04-19 14:18:35 -0400

    Create neural_network.py

---
+++ 1_Neural_net_Floris/neural_network.py
@@ -0,0 +1,11 @@
+## importing required packages
+import pandas as pd
+import numpy as np
+from sklearn.neural_network import MLPClassifier
+import matplotlib.pyplot as plt
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import RandomizedSearchCV
+from matplotlib import cm
+import seaborn
+
+
